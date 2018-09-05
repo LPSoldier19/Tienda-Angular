0b56f6abb565450728595133190cf2f0bd9f680d
LPSoldier19 <rafael.bautista1@hotmail.es>
2018-09-05 17:48:41 -0600

    Proyecto ReactJS

---
+++ react-tienda/components/Tienda.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+import update from 'immutability-helper'; //Manejo de arrays
+import * as firebase from 'firebase';
+import BarraNavegacion from './tienda/BarraNavegacion.jsx';
+//import CatalogoRow from './tienda/CatalogoRow.jsx';
+import Catalogo from './tienda/Catalogo.jsx';
+
+
+class Tienda extends React.Component{
+
+
+constructor(props) {
+  super(props)
+    this.state = {
+      catalogo: [],
+      productos: [],
+      listaCarrito : [],
+    }
+    /*this.filtrarCatalogo = this.filtrarCatalogo.bind(this);
+    this.actualizarDisponible = this.actualizarDisponible.bind(this)
+    this.obtenerCantidad = this.obtenerCantidad.bind(this)*/
+}
+
+
+
+  render(){
+    return(
+     <p>test</p>
+    )
+  }
+
+}
+
+export default Tienda;
+
